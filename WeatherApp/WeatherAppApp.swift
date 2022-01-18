7b04cb550f23efa7d6c6693de48a462cbc16ba5f
David Garza <davidagarza247@gmail.com>
2022-01-18 17:46:05 -0600

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by David Garza on 1/18/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
